acf9ef587d7a9411228c0d3704518e6d165946f2
Konka Lokesh <konkalokesh372@gmail.com>
2025-06-10 16:35:18 +0530

    advanced js asynchronous

--- JSNOTES2/ASYNC.js
+++ JSNOTES2/ASYNC.js
@@ -3,3 +3,31 @@
 // CALL STACK and MEMORY HEAP  is nothing but JS engine
 //Blocking codes(read sync files) and non blocking codes(reads async files)
 //every things have thier own usecases
+
+// const { log } = require("async");
+
+
+// 
+// network calls , timee functions, read / write files , user Input etc...
+const sayhello= ()=>{
+    console.log("say hello");
+    
+}
+setTimeout(()=>{
+    sayhello();
+},2000)
+
+// Closures
+ function outer(){
+    let c =5;
+    return function (){
+        c++;
+        return c;
+    }
+ }
+ let count = outer()
+//  console.log(count());  6
+//  console.log(count());  7
+//  console.log(count());  8
+//  console.log(count());  9
+ 
